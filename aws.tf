5e9c3c293596708f1e526614bb80cb36b654c572
Victoria Yaakov <victoriaja1916@gmail.com>
2024-05-03 07:03:40 +0300

    added eks cluster with alb and helm chart hello world

---
+++ aws.tf
@@ -0,0 +1,133 @@
+locals {
+  cluster_name = "eks-vic"
+}
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "education-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
+
+
+data "aws_eks_cluster" "cluster" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
+}
+
+
+# Role
+data "aws_iam_policy_document" "kubernetes_alb_controller_assume" {
+  count = var.enabled ? 1 : 0
+
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [var.cluster_identity_oidc_issuer_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(var.cluster_identity_oidc_issuer, "https://", "")}:sub"
+
+      values = [
+        "system:serviceaccount:${var.namespace}:${var.service_account_name}",
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(var.cluster_identity_oidc_issuer, "https://", "")}:aud"
+
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "kubernetes_alb_controller" {
+  count              = var.enabled ? 1 : 0
+  name               = "${local.cluster_name}-alb-controller"
+  assume_role_policy = data.aws_iam_policy_document.kubernetes_alb_controller_assume[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "kubernetes_alb_controller" {
+  count      = var.enabled ? 1 : 0
+  role       = aws_iam_role.kubernetes_alb_controller[0].name
+  policy_arn = aws_iam_policy.kubernetes_alb_controller[0].arn
+}
---
+++ helm.tf
@@ -0,0 +1,109 @@
+provider "helm" {
+  kubernetes {
+    host = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+resource "kubernetes_namespace" "vic-ns" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "helm_release" "hello_world" {
+  name       = "hello"
+  repository = "https://cloudecho.github.io/charts/"
+  chart      = "hello"
+  namespace = kubernetes_namespace.vic-ns.metadata[0].name
+
+  values = [
+    file("${path.module}/hello-values.yaml")
+  ]
+}
+
+data "kubernetes_ingress_v1" "ingress_hostname" {
+  metadata {
+    name = "hello"
+    namespace = kubernetes_namespace.vic-ns.metadata[0].name
+  }
+}
+
+output "kubernetes_ingress" {
+  description   = "External DN name of load balancer"
+  value         = data.kubernetes_ingress_v1.ingress_hostname.status.0.load_balancer.0.ingress.0.hostname
+}
+
+
+resource "helm_release" "alb-controller" {
+ name       = "aws-load-balancer-controller"
+ repository = "https://aws.github.io/eks-charts"
+ chart      = "aws-load-balancer-controller"
+ namespace  = kubernetes_namespace.vic-ns.metadata[0].name
+
+ set {
+     name  = "region"
+     value = var.aws_region
+ }
+
+ set {
+     name  = "vpcId"
+     value = module.vpc.vpc_id
+ }
+
+ set {
+     name  = "image.repository"
+     value = "602401143452.dkr.ecr.${var.aws_region}.amazonaws.com/amazon/aws-load-balancer-controller"
+ }
+
+ set {
+     name  = "serviceAccount.create"
+     value = "true"
+ }
+
+ set {
+    name  = "rbac.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = var.service_account_name
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.kubernetes_alb_controller[0].arn
+  }
+
+  set {
+    name  = "enableServiceMutatorWebhook"
+    value = "true"
+  }
+
+ set {
+     name  = "clusterName"
+     value = local.cluster_name
+ }
+ }
+
+ resource "aws_iam_policy" "kubernetes_alb_controller" {
+  depends_on  = [var.mod_dependency]
+  count       = var.enabled ? 1 : 0
+  name        = "${local.cluster_name}-alb-controller"
+  path        = "/"
+  description = "Policy for load balancer controller service"
+
+  policy = file("${path.module}/alb_controller_iam_policy.json")
+}
