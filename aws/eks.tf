a97c760b7cb17f7867554b11c1fe2019a9864b56
Victoria Yaakov <victoriaja1916@gmail.com>
2024-05-05 11:14:10 +0300

    changed dir

--- aws/eks.tf
+++ aws/eks.tf
@@ -1,7 +1,7 @@
 locals {
   cluster_name = "eks-vic"
 }
-# Infra: VPC, Subnets, Security Groups and EKS
+# Infra for the EKS: VPC, Subnets, Security Groups and EKS
 data "aws_availability_zones" "available" {
   filter {
     name   = "opt-in-status"
@@ -83,51 +83,4 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = local.cluster_name
-}
-
-
-# Roles for ALB controller
-data "aws_iam_policy_document" "kubernetes_alb_controller_assume" {
-  count = var.enabled ? 1 : 0
-
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
-    principals {
-      type        = "Federated"
-      identifiers = [var.cluster_identity_oidc_issuer_arn]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(var.cluster_identity_oidc_issuer, "https://", "")}:sub"
-
-      values = [
-        "system:serviceaccount:${var.namespace}:${var.service_account_name}",
-      ]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(var.cluster_identity_oidc_issuer, "https://", "")}:aud"
-
-      values = [
-        "sts.amazonaws.com"
-      ]
-    }
-
-    effect = "Allow"
-  }
-}
-
-resource "aws_iam_role" "kubernetes_alb_controller" {
-  count              = var.enabled ? 1 : 0
-  name               = "${local.cluster_name}-alb-controller"
-  assume_role_policy = data.aws_iam_policy_document.kubernetes_alb_controller_assume[0].json
-}
-
-resource "aws_iam_role_policy_attachment" "kubernetes_alb_controller" {
-  count      = var.enabled ? 1 : 0
-  role       = aws_iam_role.kubernetes_alb_controller[0].name
-  policy_arn = aws_iam_policy.kubernetes_alb_controller[0].arn
 }
